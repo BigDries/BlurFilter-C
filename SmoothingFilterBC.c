254dbdc773c77b6f2837f9a031a63d53d0b534fe
BigDries <12002476@student.pxl.be>
2022-05-27 10:42:33 +0200

    still trying to merge my 2 projects

--- SmoothingFilterBC.c
+++ SmoothingFilterBC.c
@@ -125,6 +125,67 @@ void blurfilter()
 /*
 void BlackAndWhitefilter()
 {
+    #include<stdio.h>
+    #include<stdlib.h>
+
+    #include "image.h"
+
+    int main(int argc,char *argv[])
+    {
+        char filename[100];
+        scanf("%s",filename);
+
+        FILE *fp = fopen(filename,"rb");
+        BMP_Header bmp_header = getBMP_Header(fp);
+        imageHeader imgHeader = getImageHeader(fp);
+        Image img = getImage(fp,bmp_header.offset,imgHeader.width,imgHeader.height);
+
+
+        //createBlackAndWhiteImage(img,bmp_header,imgHeader);
+        createFlippedImage(img,bmp_header,imgHeader);
+        freeImage(img);
+        return 0;
+    }
+
+    BMP_Header getBMP_Header(FILE *fp)
+    {
+
+    imageHeader getImageHeader(FILE *fp)
+    {
+        imageHeader imageHeader;
+        fread(&imageHeader,1*sizeof(struct imageHeader),1,fp);
+
+        if(imageHeader.headerSize!=124 || imageHeader.colorPanes!=1){
+            fclose(fp);
+            printf("Header size not correct!\n");
+            exit(-1);
+        }
+        return imageHeader;
+    }
+    Image getImage(FILE *fp,int offset,int width,int height)
+    {
+
+        fseek(fp,offset,SEEK_SET);
+        Image image = newImage(fp,width,height);
+        fclose(fp);
+        return image;
+    }
+
+
+    Image newImage(FILE *fp,int width,int height)
+    {
+        Image image;
+        image.height = height;
+        image.width = width;
+        image.rgb =(struct RGB**) malloc(height*sizeof(void *));
+        for(int i =height-1; i >=0 ;i--){
+            image.rgb[i] = (struct RGB*) malloc(width*sizeof(struct RGB));
+            fread(image.rgb[i],width,sizeof(struct RGB),fp);
+    }
+
+        return image;
+    }
+
     unsigned char greyScalePixel(RGB rgb)
     {
         return ((rgb.red*0.3)+(rgb.green*0.6)+(rgb.blue*0.1));
@@ -144,25 +205,72 @@ void BlackAndWhitefilter()
 
     }
 
-    FILE *fpw = fopen("bw-pic.bmp","wb");
-    if(fpw == NULL)
+    void flipImage(Image image)
     {
-        return;
+        for(int i=0;i<image.height;i++)
+        {
+            for(int j =0;j<image.width/2;j++)
+            {
+                RGB temp = image.rgb[i][j];
+                image.rgb[i][j] = image.rgb[i][image.width-j-1];
+                image.rgb[i][image.width-j-1] = temp;
+            }
+        }
     }
 
-    imageToGrayScale(image);
-
-    fwrite(bmp_header.name,2*sizeof(char),1,fpw);
-    fwrite(&bmp_header.size,3*sizeof(int),1,fpw);
-
-    fwrite(&image_header,sizeof(struct imageHeader),1,fpw);
-
-
-    for(int i=image.height-1; i>=0;i--)
+    void createBlackAndWhiteImage(Image image,BMP_Header bmp_header,imageHeader image_header)
     {
-        fwrite(image.rgb[i],image.width,sizeof(struct RGB),fpw);
+        FILE *fpw = fopen("bw-pic.bmp","wb");
+        if(fpw == NULL)
+        {
+            return;
+        }
+
+        imageToGrayScale(image);
+
+        fwrite(bmp_header.name,2*sizeof(char),1,fpw);
+        fwrite(&bmp_header.size,3*sizeof(int),1,fpw);
+
+        fwrite(&image_header,sizeof(struct imageHeader),1,fpw);
+
+
+        for(int i=image.height-1; i>=0;i--)
+        {
+            fwrite(image.rgb[i],image.width,sizeof(struct RGB),fpw);
+        }
+        fclose(fpw);
     }
-    fclose(fpw);
+
+    void freeImage(Image image)
+    {
+        for(int i=image.height-1;i>=0;i--)
+        {
+            free(image.rgb[i]);
+        }
+        free(image.rgb);
+    }
+
+    void createFlippedImage(Image image,BMP_Header bmp_header,imageHeader image_header)
+    {
+         FILE *fpw = fopen("flipped.bmp","wb");
+
+        if(fpw == NULL)
+        {
+            return ;
+        }
+        flipImage(image);
+
+        fwrite(bmp_header.name,2*sizeof(char),1,fpw);
+        fwrite(&bmp_header,3*sizeof(int),1,fpw);
+
+        fwrite(&image_header,sizeof(struct imageHeader),1,fpw);
+
+        for(int i=image.height-1; i>=0;i--)
+        {
+            fwrite(image.rgb[i],image.width,sizeof(struct RGB),fpw);
+        }
+        fclose(fpw);
+    }
 }
 
 void flipbmpfilter(image image)
